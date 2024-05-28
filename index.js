98f6d64e9901d1cfd8781155f26dce8c7027453b
Vlad Sumar <sumarvaldislav@gmail.com>
2024-05-28 18:44:53 +0300

    Done HM-9

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let taskOne = document.querySelector('#headerTwo');
+console.log(taskOne);
+
+let taskTwo = document.querySelector('h1 ~ section');
+console.log(taskTwo);
+
+let taskThree = document.querySelector('ul > li:nth-child(5)');
+console.log(taskThree);
+
+let taskFour = document.querySelector('.hatredLevelBlock');
+console.log(taskFour);
